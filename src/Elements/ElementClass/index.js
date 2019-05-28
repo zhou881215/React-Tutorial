8a0a7d663d3cb78af0f517b25c047e88a1ebbb2a
z00402tb <mingye.zhou.ext@siemens.com>
2019-05-28 17:03:27 +0800

    class element

--- src/Elements/ElementClass/index.js
+++ src/Elements/ElementClass/index.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 
-function ClassElement() {
-  return (
-    <div className="wrapper">
-      <div className="search-title-box">
-        <h5 className="search-title">JSX-Base 搜索热点</h5>
-        <span className="refresh">换一换</span>
-      </div>
-    </div>
-  );
+class ClassElement extends Component {
+  // es7语法
+  state = {
+    inValue: "",
+    list: []
+  };
+
+  onChange = e => {
+    this.setState({
+      inValue: e.target.value
+    });
+  };
+
+  onAdd = () => {
+    const { inValue, list } = this.state;
+    this.setState({
+      inValue: "",
+      list: [...list, inValue]
+    });
+  };
+
+  onRemove = index => {
+    const { list } = this.state;
+    list.splice(index, 1);
+    this.setState({
+      list
+    });
+  };
+
+  render() {
+    const { inValue, list } = this.state;
+    return (
+      <>
+        <div>TodoList: 类组建</div>
+        <div>
+          <input type="text" value={inValue} onChange={this.onChange} />
+          <button onClick={this.onAdd}>ADD</button>
+        </div>
+        <ul>
+          {list.map((item, index) => (
+            <li key={item + index}>
+              {item} <button onClick={() => this.onRemove(index)}>X</button>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
 
 export default ClassElement;
